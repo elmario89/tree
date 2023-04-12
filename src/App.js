0af56386fdb9981b7738fe15ee424f79f3ddb762
Mikhail <urine89@mail.ru>
2023-04-12 12:34:52 +0300

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import BinaryNodeTreeApp from "./components/BinaryNodeTreeApp";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <div
+              className="repos"
+              style={{
+                position: "absolute",
+                top: 10,
+                left: 10,
+                border: 0
+              }}
+          >
+            <a
+                href="https://github.com/boxgames1/hash-tables-app"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              <button
+                  className="btn btn-default"
+                  style={{
+                    marginRight: 10
+                  }}
+              >
+                Hash Tables
+              </button>
+            </a>
+            <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/boxgames1/double-linked-list-app"
+            >
+              <button
+                  className="btn btn-default"
+                  style={{
+                    marginRight: 10
+                  }}
+              >
+                Double Linked Lists
+              </button>
+            </a>
+            <a
+                href="https://github.com/boxgames1/binary-trees-app"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              <button
+                  className="btn btn-default"
+                  style={{
+                    marginRight: 10
+                  }}
+              >
+                Binary Trees
+              </button>
+            </a>
+          </div>
+          <header className="App-header">
+            <h1 className="App-title">
+              Дерево
+            </h1>
+          </header>
+          <BinaryNodeTreeApp />
+        </div>
+    );
+  }
 }
 
 export default App;
